a49b9ec21ecc3d8005f08a92b72e7811287f6fb7
Davidb2303 <dballesteros2303@gmail.com>
2025-02-09 00:07:15 -0500

    Revert "Organizate"
    This reverts commit 9d324e4f2a26b2508f27933c458f1f37b89b76bc.

--- Paneles Solares/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-
-    public static void main(String[] args) throws InterruptedException {
-        // Creamos una instancia del controlador y lo iniciamos
-        @SuppressWarnings("rawtypes")
-        Controller controller = new Controller();
-        controller.start();
-    }
-}
